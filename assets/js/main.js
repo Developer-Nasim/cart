c960000e1e59391bc98d0e40663c7880518df4b5
Developer-Nasim <ajnasim72@gmail.coom>
2023-02-23 11:57:40 +0600

    up

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+
+
+  // owlCarousel
+  $(".services_wrp").owlCarousel({
+    loop: false,
+    margin: 10,
+    items: 2,
+    stagePadding: 20,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false
+  });
+
+
+  // owlCarousel
+  $(".Testimonial_Slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: true
+  });
+
+  // owlCarousel
+  $(".brand-active").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 6,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ],
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      767: {
+        items: 3
+      },
+      992: {
+        items: 6
+      }
+    }
+  });
+
+ 
+})(jQuery);
